bcdbbefcfd24826efa8282bfafa974a554cb3ec3
bogdan <bogdanbogdanvakulenka@gmail.com>
2025-05-16 18:21:37 +0300

    memento: undo, redo logic; task state

--- src/core/patterns/memento/History.java
+++ src/core/patterns/memento/History.java
@@ -1,4 +1,40 @@
 package core.patterns.memento;
 
-public class Memento {
+import models.Task;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Stack;
+
+public class History {
+    private final Stack<TaskMemento> undoStack = new Stack<>();
+    private final Stack<TaskMemento> redoStack = new Stack<>();
+
+    /**
+     * Метод додає новий масів в історію
+     * */
+    public void push(TaskMemento memento) {
+        undoStack.push(memento);
+        redoStack.clear();
+    }
+
+    /**
+     * Метод який видаляє останнє значення з масиву
+     * */
+    public TaskMemento undo(ArrayList<Task> currentState) {
+        if (undoStack.isEmpty()) return null;
+        TaskMemento previous = undoStack.pop();
+        redoStack.push(new TaskMemento(new ArrayList<>((Collection) currentState)));
+        return previous;
+    }
+
+    /**
+     * Метод який повертає останнє значення в масиву
+     * */
+    public TaskMemento redo(ArrayList<Task> currentState) {
+        if (redoStack.isEmpty()) return null;
+        TaskMemento next = redoStack.pop();
+        undoStack.push(new TaskMemento(new ArrayList<>((Collection) currentState)));
+        return next;
+    }
 }
--- src/core/patterns/memento/TaskMemento.java
+++ src/core/patterns/memento/TaskMemento.java
@@ -1,4 +1,22 @@
 package core.patterns.memento;
 
+import models.Task;
+
+import java.util.ArrayList;
+
+/**
+ * Класс в якому буду зберігаться стейт
+ * В мойому випадку наверное це буде
+ * ArrayList<Task> tasks або task.getDescription()
+ * */
 public class TaskMemento {
+    private final ArrayList<Task> state;
+
+    public TaskMemento(ArrayList<Task> state) {
+        this.state = new ArrayList<>(state);
+    }
+
+    public ArrayList<Task> getState() {
+        return state;
+    }
 }
